339a32983f89fa1147a3d5f155ec5c757909639e
Sriram V <sriram.vasudevan.93@gmail.com>
2012-08-05 17:54:46 +0530

    First push

---
+++ lab1/problem1/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: sriram
+ *
+ * Created on 5 August, 2012, 5:52 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    return (EXIT_SUCCESS);
+}
+
