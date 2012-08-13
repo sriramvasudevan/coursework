12e3a92d43e34326b6dd12177c4c7947321c27ae
Sriram <sriram.vasudevan.93@gmail.com>
2012-08-13 15:54:23 +0530

    Untracked some more

--- lab2/NetworkSeries_1/build/Debug/GNU-Linux-x86/main.o.d
+++
@@ -1,7 +0,0 @@
-build/Debug/GNU-Linux-x86/main.o: main.c singly.c singly.h packet.h
-
-singly.c:
-
-singly.h:
-
-packet.h:
--- lab2/NetworkSeries_1/build/Debug/GNU-Linux-x86/singly.o.d
+++
@@ -1,5 +0,0 @@
-build/Debug/GNU-Linux-x86/singly.o: singly.c singly.h packet.h
-
-singly.h:
-
-packet.h:
