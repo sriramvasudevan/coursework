5b4c567536872904f94339b4839a89b70049cbe4
Sriram V <sriram.vasudevan.93@gmail.com>
2013-09-22 17:07:10 +0530

    Most of PoP Assignment 2 done

--- cs3100/Assignment 2/assignment.py
+++ cs3100/Assignment 2/assignment.py
@@ -1,109 +1,142 @@
 import sys
 from collections import deque
+from random import random
+from math import ceil
 
 
-class Person:
+class Entity(object):
     number = 0
-    def __init__(self, goods):
-        Person.number += 1
-        self.id = Person.number
-        self.goods_desired = goods
+    def __init__(self, max_goods_desired, desire_dist, **kwargs):
+        Entity.number += 1
+        self.id = Entity.number
+        self.goods_desired = ceil(max_goods_desired*desire_dist(**kwargs))
         self.curr_goods = 0
         self.exit = False
 
 
-class Counter:
+class Counter(object):
     number = 0
-    def __init__(self, goods):
+    def __init__(self, max_goods, goods_dist, **kwargs):
         Counter.number += 1
         self.id = Counter.number
-        self.goods = goods
+        self.goods = ceil(max_goods*goods_dist(**kwargs))
         self.engaged = None
         self.empty = (self.goods==0)
 
 
-# Global variables
-counters = []
-for i in range(1,3):
-    counter = Counter(5)
-    counters.append(counter)
-
-time = 0
-delay = 2
-queue = deque()
-exit_queue = deque()
-
-
-def exitQueue():
-    global exit_queue
-    for person in exit_queue:
-        if person.goods_desired:
-            person.goods_desired -= 1
-        else:
-            person.exit = True
-            print person.id, " exiting queue at t = ", time
-
-    exit_queue = deque([person for person in exit_queue if not person.exit])
+class QueueSimulator(object):
+    def __init__(self, Counter=Counter, Entity=Entity):
+        # Global variables
+        self.Counter = Counter
+        self.Entity = Entity
+        self.counters = []
+        self.time = 0
+        self.entry_queue = deque()
+        self.exit_queue = deque()
 
 
-def serveAtCounter():
-    global counters
-    for counter in counters:
-        if counter.engaged:
-            if (counter.engaged.goods_desired != counter.engaged.curr_goods):
+    def initCounters(self,  no_counters=3, max_goods=5, goods_dist=(lambda : 1), **kwargs):
+        for i in range(1, no_counters):
+            counter = self.Counter(max_goods, goods_dist, **kwargs)
+            self.counters.append(counter)
+        print len(self.counters), "counters created at t =", self.time
+
+
+    def exitQueue(self):
+        # Is it a queue with a min wait time, or instantaneously leaving?
+        # Do the people waiting in the entry queue also need to leave?
+        for entity in self.exit_queue:
+            # No need to check if curr_goods is non-zero because it will be.
+            # If curr_goods was zero, it never got anything from the counters (they were all closed)
+            # which means such an entity is one that has directly been sent to exit queue.
+            # This is handled elsewhere.
+            entity.curr_goods -= 1
+            if not entity.curr_goods:
+                entity.exit = True
+                print entity.id, "exiting queue at t =", self.time
+
+        self.exit_queue = deque([entity for entity in self.exit_queue if not entity.exit])
+        if not self.exit_queue:
+            print "Exit queue empty at t =", self.time
+
+
+    def serveAtCounter(self):
+        for counter in self.counters:
+            if counter.engaged:
+                #If an entity is at the counter, then he is definitely unsatisfied
+                #Hence no need to check if curr==desired here
                 if counter.goods:
                     counter.goods -= 1
                     counter.engaged.curr_goods += 1
-                else: #Just got empty in the previous iteration, macha still at counter
+                    if (counter.engaged.goods_desired == counter.engaged.curr_goods):
+                        if not counter.goods:
+                            counter.empty = True
+                        self.exit_queue.append(counter.engaged)
+                        print counter.engaged.id, " joining exit queue at t = ", self.time
+                        counter.engaged = None
+
+                else: #Just got empty in the previous iteration, entity still at counter
                     counter.empty = True
-                    exit_queue.append(counter.engaged)
-                    print counter.engaged.id, " joining exit queue at t = ", time
+                    self.exit_queue.append(counter.engaged)
+                    print counter.engaged.id, " joining exit queue at t = ", self.time
                     counter.engaged = None
+
+        self.counters = [counter for counter in self.counters if not counter.empty]
+        if not self.counters:
+            print "Counters empty at t = ", self.time
+
+
+    def moveToCounter(self):
+        for counter in self.counters:
+            if not counter.engaged:
+                if self.entry_queue:
+                    counter.engaged = self.entry_queue.popleft()
+                    print counter.engaged.id, " going to counter ", counter.id, " at t = ", self.time
+                else:
+                    break
+
+
+    def initQueue(self, max_goods_desired=4, desire_dist=(lambda : 1), **kwargs):
+        #Find out what sir said
+        total = sum([counter.goods for counter in self.counters])
+        while total:
+            entity = self.Entity(max_goods_desired, desire_dist, **kwargs)
+            self.entry_queue.append(entity)
+            total -= 1
+            print entity.id, " Joining queue at t = ", self.time
+
+
+    def viewpoint():
+        pass
+
+
+    def startSimulation(self, argv):
+        # Going in reverse to prevent same entity from moving
+        # forward multiple steps in one time instance
+        while self.counters:
+            self.exitQueue()
+            self.serveAtCounter()
+            if self.counters:
+                self.moveToCounter()
             else:
-                if not counter.goods:
-                    counter.empty = True
-                exit_queue.append(counter.engaged)
-                print counter.engaged.id, " joining exit queue at t = ", time
-                counter.engaged = None
+                print "Emptying entry queue into exit queue at t = ", self.time
+                while self.entry_queue:
+                    entity = self.entry_queue.popleft()
+                    self.exit_queue.append(entity)
+                    print entity.id, " joining exit queue at t = ", self.time
+                    entity.exit = True
+                    print entity.id, "exiting queue at t =", self.time
+                self.exit_queue = deque([entity for entity in self.exit_queue if not entity.exit])
+            self.time += 1
 
-    counters = [counter for counter in counters if not counter.empty]
-
-
-def moveToCounter():
-    for counter in counters:
-        if not counter.engaged:
-            if queue:
-                counter.engaged = queue.popleft()
-                print counter.engaged.id, " going to counter ", counter.id, " at t = ", time
-            else:
-                break
-
-
-def joinQueue():
-    if (time%delay==0):
-        person = Person(4)
-        queue.append(person)
-        print person.id, " Joining queue at t = ", time
-
-
-def main(argv):
-# Going in reverse to prevent same entity from moving
-# forward multiple steps in one time instance
-    global time
-    while counters:
-        exitQueue()
-        serveAtCounter()
-        if counters:
-            moveToCounter()
-            joinQueue()
-        time += 1
-    print "Counters empty at t = ", time
-    print "Emptying exit queue"
-    while exit_queue:
-        exitQueue()
-        time += 1
-    print "Exit queue empty at t = ", time
+        print "Waiting for exit queue to empty"
+        while self.exit_queue:
+            self.exitQueue()
+            self.time += 1
 
 
 if __name__ == "__main__":
-    main(sys.argv)
+    queue_simulator = QueueSimulator()
+    queue_simulator.initCounters()
+    queue_simulator.initQueue()
+    queue_simulator.startSimulation(sys.argv)
